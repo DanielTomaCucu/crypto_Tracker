80bf0c2e5635311717c773e6d8a6ee47bb2bbc45
Cucu Toma Daniel <cucutomadaniel@Cucus-MBP.domain.name>
2023-09-10 15:22:36 +0300

    Removed files that are now in .gitignore

--- crypto_tracker/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  messariApiKey: '-1IXvrY5mzqLOPfv+UM1AD3FcgDsCh1nWIaTg3-ULv79CUrm',
-};
--- crypto_tracker/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  messariApiKey: '-1IXvrY5mzqLOPfv+UM1AD3FcgDsCh1nWIaTg3-ULv79CUrm',
-};
