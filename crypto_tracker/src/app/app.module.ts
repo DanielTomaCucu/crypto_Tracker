8475afe07bc41c02531906a3ad3efcb1541e7c27
Cucu Toma Daniel <cucutomadaniel@192-168-0-239.rdsnet.ro>
2023-09-12 21:17:49 +0300

    add pagination and right number for all tokens

--- crypto_tracker/src/app/app.module.ts
+++ crypto_tracker/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CryptoListComponent } from './crypto-list/crypto-list.component';
@@ -9,6 +8,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
 import { TradingViewWidgetComponent } from './crypto-list/trading-view-widget/trading-view-widget.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
 @NgModule({
   declarations: [AppComponent, CryptoListComponent, TradingViewWidgetComponent],
   imports: [
@@ -18,6 +18,7 @@ import { TradingViewWidgetComponent } from './crypto-list/trading-view-widget/tr
     MatIconModule,
     BrowserAnimationsModule,
     MatTableModule,
+    MatPaginatorModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
