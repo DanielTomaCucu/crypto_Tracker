47bd4f3a6742048214f4e5d52040ba3758c1daf9
Cucu Toma Daniel <cucutomadaniel@192-168-0-239.rdsnet.ro>
2023-09-12 10:22:41 +0300

    add list of crypto on main page

--- crypto_tracker/src/app/app.module.ts
+++ crypto_tracker/src/app/app.module.ts
@@ -5,9 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CryptoListComponent } from './crypto-list/crypto-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
 @NgModule({
   declarations: [AppComponent, CryptoListComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- crypto_tracker/src/app/crypto-list/crypto-list.component.ts
+++ crypto_tracker/src/app/crypto-list/crypto-list.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CryptoListService } from './crypto-list.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-crypto-list',
@@ -8,11 +10,31 @@ import { CryptoListService } from './crypto-list.service';
 })
 export class CryptoListComponent {
   cryptoList: any;
+  @ViewChild(MatSort) sort!: MatSort;
   constructor(private cryptoListService: CryptoListService) {}
+
+  displayedColumns: string[] = [
+    'name',
+    'price',
+    'oneH',
+    'oneDay',
+    'mcap',
+    'volume',
+  ];
+  dataSource!: MatTableDataSource<any>;
+
   ngOnInit() {
     this.cryptoListService.getAssets().subscribe((data) => {
       console.log(data.data);
       this.cryptoList = data.data;
+      this.dataSource = new MatTableDataSource(data.data);
+      this.dataSource.sort = this.sort;
     });
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+
   }
 }
